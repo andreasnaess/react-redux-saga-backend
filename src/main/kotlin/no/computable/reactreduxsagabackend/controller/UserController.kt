ce7aca094e3961b23992c9b645c18af5e512937c
Andreas Næss <andreas.n92@gmail.com>
2022-06-12 20:55:54 +0200

    Added Dockerfile

--- src/main/kotlin/no/computable/reactreduxsagabackend/controller/UserController.kt
+++ src/main/kotlin/no/computable/reactreduxsagabackend/controller/UserController.kt
@@ -1,10 +1,14 @@
 package no.computable.reactreduxsagabackend.controller
 
+import mu.KotlinLogging
 import no.computable.reactreduxsagabackend.dto.UserDTO
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+
+private val logger = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping("/api")
@@ -12,5 +16,8 @@ class UserController(
     private val restTemplate: RestTemplate
 ) {
     @GetMapping("/users")
-    fun users() = restTemplate.getForEntity("https://jsonplaceholder.typicode.com/users", Array<UserDTO>::class.java)
+    fun users(): ResponseEntity<Array<UserDTO>> {
+        logger.info { "Henter brukere" }
+        return restTemplate.getForEntity("https://jsonplaceholder.typicode.com/users", Array<UserDTO>::class.java)
+    }
 }
